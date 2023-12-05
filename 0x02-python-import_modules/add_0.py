bfb3a9cd2ac7b686ed84a46d8bddb186aca3d604
BigT001 <unclet1992@gmail.com>
2023-12-05 02:43:37 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+        my addition function
+
+        Args:
+            a: first interger
+            b: second integer
+
+        Return:
+            The return value. a + b
+    """
+
+    return (a + b)
