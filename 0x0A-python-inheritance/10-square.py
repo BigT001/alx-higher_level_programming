c994df9135453a919f879d494ee764ad904bdbf2
BigT001 <unclet1992@gmail.com>
2024-01-08 14:29:51 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class BaseGeometry:
+    """BaseGeometry class
+    """
+    def area(self):
+        """Raises an Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Square(Rectangle):
+    """Square class
+    """
+    def __init__(self, size):
+        """Instantiation
+        """
+        self.__size = size
+        BaseGeometry.integer_validator(self, "size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns area
+        """
+        return self.__size ** 2
